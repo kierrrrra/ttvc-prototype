01e931f2624e9d3be6d0affad6ada6bf014bc711
Sergey Chernyshev <schernyshev@etsy.com>
2022-10-03 10:31:07 -0400

    TTVC prototype

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { init, getTTVC } from '@dropbox/ttvc';
+
+// Call this as early in pageload as possible to setup instrumentation.
+init({
+    debug: false,
+    idleTimeout: 2000,
+    networkTimeout: 0,
+});
+
+// Reports the last visible change for each navigation that
+// occurs during the life of this document.
+const unsubscribe = getTTVC((measurement) => {
+    console.log('TTVC:', measurement.duration);
+});
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, './src'),
+    entry: './index.js',
+    mode: 'production',
+    target: 'web',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'webpack.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html', scriptLoading: 'blocking'
+        }),
+        new HtmlInlineScriptPlugin()
+    ],
+};
